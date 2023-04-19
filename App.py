1a1aadb3933c58d20f5f8e44e4c5fe0e79b8001f
MattGuil <guillemin.matt@gmail.com>
2023-04-19 12:12:26 -0400

    create basic form

--- App.py
+++ App.py
@@ -1,103 +1,134 @@
 import tkinter as tk
-from pathlib import Path
 from tkinter import ttk
 
-from tkinterdnd2 import DND_FILES, TkinterDnD
+root = tk.Tk()
 
-import pandas as pd
+root.title("Formulaire Titanic")
 
+def on_entry_click(event):
+    if input_cabin.get() == "Ex: A-Z 0-999":
+        input_cabin.delete(0, "end")
+        input_cabin.config(fg = 'black')
 
-class Application(TkinterDnD.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("CSV Viewer")
-        self.main_frame = tk.Frame(self)
-        self.main_frame.pack(fill="both", expand="true")
-        self.geometry("900x500")
-        self.search_page = SearchPage(parent=self.main_frame)
+def on_focusout(event):
+    if input_cabin.get() == "":
+        input_cabin.insert(0, "Ex: A-Z 0-999")
+        input_cabin.config(fg = 'grey')
 
+def valider():
+    lastname = input_lastname.get()
+    firstname = input_firstname.get()
+    pclass = input_pclass.get()
+    sex = input_sex.get()
+    age = input_age.get()
+    sibsp = input_sibsp.get()
+    parch = input_parch.get()
+    fare = input_fare.get()
+    cabin = input_cabin.get()
+    embarked = input_embarked.get()
 
-class DataTable(ttk.Treeview):
-    def __init__(self, parent):
-        super().__init__(parent)
-        scroll_Y = tk.Scrollbar(self, orient="vertical", command=self.yview)
-        scroll_X = tk.Scrollbar(self, orient="horizontal", command=self.xview)
-        self.configure(yscrollcommand=scroll_Y.set, xscrollcommand=scroll_X.set)
-        scroll_Y.pack(side="right", fill="y")
-        scroll_X.pack(side="bottom", fill="x")
-        self.stored_dataframe = pd.DataFrame()
+    print("Identité : ", lastname + " " + firstname)
+    print("Classe :", pclass)
+    print("Genre :", sex)
+    print("Age :", age)
+    print("Nombre de frères et sœurs / conjoints à bord :", sibsp)
+    print("Nombre de parents / enfants à bord :", parch)
+    print("Tarif du billet :", fare)
+    print("Cabine :", cabin)
+    print("Port d'embarquement :", embarked)
 
-    def set_datatable(self, dataframe):
-        self.stored_dataframe = dataframe
-        self._draw_table(dataframe)
+# Ajout d'un label pour le champ "Prénom"
+label_firstname = tk.Label(root, text="Prénom : ")
+label_firstname.pack()
 
-    def _draw_table(self, dataframe):
-        self.delete(*self.get_children())
-        columns = list(dataframe.columns)
-        self.__setitem__("column", columns)
-        self.__setitem__("show", "headings")
+# Ajout d'un champ de saisie pour le prénom
+input_firstname = tk.Entry(root)
+input_firstname.pack()
 
-        for col in columns:
-            self.heading(col, text=col)
+# Ajout d'un label pour le champ "Nom"
+label_lastname = tk.Label(root, text="Nom : ")
+label_lastname.pack()
 
-        df_rows = dataframe.to_numpy().tolist()
-        for row in df_rows:
-            self.insert("", "end", values=row)
-        return None
+# Ajout d'un champ de saisie pour le nom
+input_lastname = tk.Entry(root)
+input_lastname.pack()
 
-    def find_value(self, pairs):
-        # pairs is a dictionary
-        new_df = self.stored_dataframe
-        for col, value in pairs.items():
-            query_string = f"{col}.str.contains('{value}')"
-            new_df = new_df.query(query_string, engine="python")
-        self._draw_table(new_df)
+# Ajout d'un label pour le champ "Pclass"
+label_pclass = tk.Label(root, text="Classe : ")
+label_pclass.pack()
 
-    def reset_table(self):
-        self._draw_table(self.stored_dataframe)
+# Ajout d'une liste déroulante pour la classe de billets
+input_pclass = ttk.Combobox(root, values=["1er classe", "2ème classe", "3ème classe"])
+input_pclass.pack()
 
+# Ajout d'un label pour le champ "Sex"
+label_sex = tk.Label(root, text="Genre : ")
+label_sex.pack()
 
-class SearchPage(tk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.file_names_listbox = tk.Listbox(parent, selectmode=tk.SINGLE, background="darkgray")
-        self.file_names_listbox.place(relheight=1, relwidth=0.25)
-        self.file_names_listbox.drop_target_register(DND_FILES)
-        self._display_file()
+# Ajout de boutons radio pour le genre du passager
+input_sex = tk.StringVar()
+input_sex.set("Homme")
+bouton_homme = tk.Radiobutton(root, text="Homme", variable=input_sex, value="Homme")
+bouton_homme.pack()
+bouton_femme = tk.Radiobutton(root, text="Femme", variable=input_sex, value="Femme")
+bouton_femme.pack()
 
-        self.search_entrybox = tk.Entry(parent)
-        self.search_entrybox.place(relx=0.25, relwidth=0.75)
-        self.search_entrybox.bind("<Return>", self.search_table)
+# Ajout d'un label pour le champ "Age"
+label_age = tk.Label(root, text="Age : ")
+label_age.pack()
 
-        # Treeview
-        self.data_table = DataTable(parent)
-        self.data_table.place(rely=0.05, relx=0.25, relwidth=0.75, relheight=0.95)
+# Ajout d'un champ de saisie pour l'âge
+input_age = tk.Entry(root)
+input_age.pack()
 
-        self.path_map = {}
+# Ajout d'un label pour le champ "SibSp"
+label_sibsp = tk.Label(root, text="Nombre de frères et sœurs / conjoints à bord : ")
+label_sibsp.pack()
 
-    def _display_file(self, event):
-        file_name = 'C:/Users/guill/OneDrive/Documents/UQAC/Forage/PROJET FINAL/Titanic-Dataset.xls'
-        path = self.path_map[file_name]
-        df = pd.read_csv(path)
-        self.data_table.set_datatable(dataframe=df)
+# Ajout d'un curseur pour le nombre de frères et soeurs / conjoints à bord
+input_sibsp = tk.Scale(root, from_=0, to=10, orient="horizontal")
+input_sibsp.pack()
 
-    def search_table(self, event):
-        # column value. [[column,value],column2=value2]....
-        entry = self.search_entrybox.get()
-        if entry == "":
-            self.data_table.reset_table()
-        else:
-            entry_split = entry.split(",")
-            column_value_pairs = {}
-            for pair in entry_split:
-                pair_split = pair.split("=")
-                if len(pair_split) == 2:
-                    col = pair_split[0]
-                    lookup_value = pair_split[1]
-                    column_value_pairs[col] = lookup_value
-            self.data_table.find_value(pairs=column_value_pairs)
+# Ajout d'un label pour le champ "Parch"
+label_parch = tk.Label(root, text="Nombre de parents / enfants à bord : ")
+label_parch.pack()
 
+# Ajout d'un curseur pour le nombre de parents / enfants à bord
+input_parch = tk.Scale(root, from_=0, to=10, orient="horizontal")
+input_parch.pack()
 
-if __name__ == "__main__":
-    root = Application()
-    root.mainloop()
+# Ajout d'un label pour le champ "Fare"
+label_fare = tk.Label(root, text="Tarif du billet : ")
+label_fare.pack()
+
+# Ajout d'un champ de saisie pour le tarif passager
+input_fare = tk.Entry(root)
+input_fare.pack()
+
+# Ajout d'un label pour le champ "Cabin"
+label_cabin = tk.Label(root, text="Cabine : ")
+label_cabin.pack()
+
+# Ajout d'un champ de saisie pour la cabine du passager
+input_cabin = tk.Entry(root)
+input_cabin.insert(0, "Ex: A-Z 0-999")
+input_cabin.pack()
+
+input_cabin.bind('<FocusIn>', on_entry_click)
+input_cabin.bind('<FocusOut>', on_focusout)
+input_cabin.config(fg = 'grey')
+
+# Ajout d'un label pour le champ "Embarked"
+label_embarked = tk.Label(root, text="Port d'embarquement : ")
+label_embarked.pack()
+
+# Ajout d'une liste déroulante pour la ville d'embarquement
+input_embarked = ttk.Combobox(root, values=["Cherbourg", "Queenstown", "Southampton"])
+input_embarked.pack()
+
+# Ajout d'un bouton pour valider le formulaire
+bouton_valider = tk.Button(root, text="Valider", command=valider)
+bouton_valider.pack()
+
+# Lancement de la boucle principale de la fenêtre
+root.mainloop()
